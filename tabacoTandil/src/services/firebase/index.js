705a31d1ccd084f7f52e4acf1e5a0e8e6a9633c3
diegoevasquez18 <71709152+diegoevasquez18@users.noreply.github.com>
2022-11-02 19:07:15 -0300

    Update

--- tabacoTandil/src/services/firebase/index.js
+++ tabacoTandil/src/services/firebase/index.js
@@ -3,15 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.measurementId
+  apiKey: "AIzaSyDFUSXtM_5HPF8p7MfbyGsVyQMxvZ7PuNw",
+  authDomain: "tabacostandil-687bc.firebaseapp.com",
+  projectId: "tabacostandil-687bc",
+  storageBucket: "tabacostandil-687bc.appspot.com",
+  messagingSenderId: "846266043674",
+  appId: "1:846266043674:web:736420a2f28c70acf9b0a3",
+  measurementId: "G-29W94RV2FW"
 };
 
 // Initialize Firebase
